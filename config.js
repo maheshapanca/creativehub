c87bc40e630f20e8c1b7f0ecb2d8d21ed4facc6c
maheshapanca <80650168+maheshapanca@users.noreply.github.com>
2026-07-11 15:49:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+window.CREATIVE_HUB_CONFIG = Object.freeze({
+  supabaseUrl: 'https://ciunbeokqipnhelcivat.supabase.co',
+  supabasePublishableKey: 'sb_publishable_hS1nKbBk648ZxpHK4o4ydQ_pvKOtKCN',
+  appVersion: 6,
+  ownerEmail: 'maheshapanca@gmail.com',
+  calendarFunctionName: 'google-calendar',
+  tables: Object.freeze({
+    umbrellas: 'ch_umbrellas',
+    projects: 'ch_projects',
+    milestones: 'ch_milestones',
+    yearPlans: 'ch_year_plans',
+    notes: 'ch_notes',
+    settings: 'ch_settings',
+    snapshots: 'ch_snapshots',
+    changeLog: 'ch_change_log'
+  })
+});
